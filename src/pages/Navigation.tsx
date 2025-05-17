3ff4ce802fc1d17dc9e4312c46fd36817bd7876f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-17 21:24:57 +0800

    refactor(Navigation): center the nav

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Navigation = () => {
   return (
-    <header className="flex justify-between items-center w-full bg-logo-text/20 p-5">
-      {/* logo */}
-      <div>Logo</div>
-      {/* nav links */}
-      <div>
-        <ul className="flex items-center space-x-10">
-          <li>Home</li>
-          <li>Tv Shows</li>
-          <li>Movies</li>
-          <li>Upcoming</li>
-        </ul>
-      </div>
-      {/* Search bar */}
-      <div>Search</div>
-    </header>
+    <div className="fixed top-0 left-0 w-full flex items-center justify-center border-b border-logo-white/20   p-5 z-10">
+      <header className=" flex justify-between items-center w-full   max-w-7xl">
+        {/* logo */}
+        <div className="font-black">MoovySpot</div>
+        {/* nav links */}
+        <div>
+          <ul className="flex items-center space-x-10 text-logo-white">
+            <li>Home</li>
+            <li>Tv Shows</li>
+            <li>Movies</li>
+            <li>Upcoming</li>
+          </ul>
+        </div>
+        {/* Search bar */}
+        <div className="flex items-center space-x-2">
+          <div className="p-2 rounded-full bg-logo-white/50">
+            <BiSearchAlt className="text-xl" />
+          </div>
+        </div>
+      </header>
+    </div>
   );
 };
 
