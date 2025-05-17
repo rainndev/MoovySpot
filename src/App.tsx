a06c5d0d5e200f879af758fdd83f5207c2b0830b
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-17 21:25:08 +0800

    refactor(Homepage): update layout and styling for improved visuals

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Homepage from "./pages/Homepage";
 import Navigation from "./pages/Navigation";
 
 const App = () => {
   return (
-    <div className="w-full h-[100dvh] bg-logo-background flex flex-col items-center justify-center text-logo-text">
+    <div className="w-full h-[100dvh] bg-logo-black flex flex-col items-center justify-center text-logo-white  ">
       <Navigation />
       <div className="h-full w-full flex items-center justify-center">
         <Homepage />
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="bg-logo-black w-full h-full">
+      <img
+        className="object-cover relative w-full h-full"
+        src="/img/sample.jpg"
+        alt=""
+      />
+      <div className="bg-radial from-40%  from-transparent to-black absolute w-full h-full inset-0 " />
+    </div>
+  );
 };
 
 export default Homepage;
