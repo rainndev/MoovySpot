1176faede062d08da5921d18e56b177198149c28
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-18 19:36:24 +0800

    refactor(Search): enhance title and description for better user engagement

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,15 +4,22 @@ const Search = () => {
   return (
     <div className="max-w-7xl p-10 flex items-center justify-center flex-col">
       {/* Title`` */}
-      <div className="font-[ClashDisplay] font-bold text-7xl text-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit
+      <div className="text-center text-pretty">
+        <h1 className="font-[ClashDisplay] font-bold text-7xl ">
+          Your Movie Night <span className="text-logo-blue">Starts Here</span>
+        </h1>
+        <p className="text-xl mt-2 ">
+          Tired of wasting time picking what to watch? MoovySpot gives you
+          trending picks, personalized lists, and curated collections—all in one
+          spot
+        </p>
       </div>
       {/* Search input */}
       <div className="w-full mt-10">
         <input
           type="text"
           placeholder="Search"
-          className="ring-1 p-3 rounded-full w-full "
+          className="ring-1 p-3 rounded-full w-full pl-10"
         />
       </div>
     </div>
