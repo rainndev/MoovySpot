0191763f2ba73d03eeb86b72ffce3e5656474d0a
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-21 11:36:57 +0800

    feat: add disable-devtool dependency and implement DevTools disabling in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import disableDevtool from "disable-devtool";
 
 const queryClient = new QueryClient();
+
+// Disable DevTools
+disableDevtool({
+  disableMenu: true, // disables right-click
+  clearLog: true, // clears console logs
+  url: "about:blank", // redirect URL if DevTools detected
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
