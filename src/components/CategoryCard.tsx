38328aced489974688c730a19857470eb808fdeb
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-31 21:05:22 +0800

    feat: add CategoryCard component for displaying movie items with lazy loading and hover effects

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import { formatImagePath, formatWatchUrl } from "@/lib/watch-utils";
+import type { MediaItem } from "@/types/TMDBTypes";
+
+interface CategoryCardProps {
+  movie: MediaItem;
+}
+const CategoryCard = ({ movie }: CategoryCardProps) => {
+  const type = movie.type;
+  if (!movie.poster_path) return null;
+  if (!movie.title && !movie.name) return null;
+
+  return (
+    <Link to={formatWatchUrl(movie.id, type)}>
+      <div
+        className={`group h-full w-full flex-shrink-0 snap-start overflow-hidden`}
+      >
+        <div className="bg-logo-black/50 relative w-full overflow-hidden rounded-lg transition-all duration-300 ease-in-out hover:shadow-xl md:rounded-2xl">
+          <div className="aspect-[3/4]">
+            <img
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+              loading="lazy"
+              src={formatImagePath(movie.poster_path, "w300")}
+              alt={movie.title || movie.name}
+              draggable="false"
+              className={`aspect-[3/4] scale-100 object-cover opacity-90 shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:opacity-50 active:scale-105 active:opacity-50`}
+            />
+          </div>
+
+          <div className="bg-logo-black/50 pointer-events-none absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,#14c4b4)]" />
+        </div>
+
+        {/* <h2 className="mt-5 truncate text-[clamp(.8rem,3vw,1.1rem)] font-normal text-white">
+          {movie.title || movie.name}
+        </h2>
+        */}
+      </div>
+    </Link>
+  );
+};
+
+export default CategoryCard;
--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,7 +1,129 @@
+import CategoryCard from "@/components/CategoryCard";
+import { useGenreOptions } from "@/query-options/QueryGenreOptions";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import type { MediaItem, MediaType } from "@/types/TMDBTypes";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import LoadingAnimation from "@/components/LoadingAnimation";
+
+interface GenreItem {
+  id: number;
+  name: string;
+}
+
+const fetchMovies = async (
+  pageParam: number = 1,
+  type: MediaType,
+  genre?: number,
+) => {
+  const res = await axios.get(`https://api.themoviedb.org/3/discover/${type}`, {
+    params: {
+      api_key: import.meta.env.VITE_TMDB_API_KEY,
+      page: pageParam,
+      with_genres: genre,
+    },
+  });
+  return res.data;
+};
+
 const CategoryPage = () => {
+  const [selectedGenre, setSelectedGenre] = useState<number>();
+  const [selectedType, setSelectedType] = useState<MediaType>("movie");
+  const { ref: loadMoreRef, inView } = useInView();
+
+  const { data: GenreList } = useGenreOptions(selectedType);
+  const { data, fetchNextPage, isLoading, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: [`${selectedType}-category-${selectedGenre}`],
+      queryFn: ({ pageParam = 1 }) =>
+        fetchMovies(pageParam, selectedType, selectedGenre),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage) => {
+        if (lastPage.page < lastPage.total_pages) {
+          return lastPage.page + 1;
+        }
+        return undefined;
+      },
+    });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, isFetchingNextPage]);
+
   return (
-    <div className="flex h-dvh w-full items-center justify-center">
-      CategoryPage
+    <div className="flex h-dvh w-full flex-col items-center p-3 pb-20 md:p-10 md:pl-25">
+      {/* header */}
+      <div className="flex w-full flex-col items-center">
+        <h1 className="mb-5 w-full text-start font-[ClashDisplay] text-[clamp(1.125rem,3vw,2rem)] font-medium">
+          Category{" "}
+          <span className="text-logo-blue">
+            {selectedType === "movie" ? "Movie" : "TV "}
+          </span>
+        </h1>
+
+        {/* option category */}
+        <ul className="text-logo-white hide-scrollbar flex w-full snap-x snap-proximity gap-1 overflow-x-scroll md:gap-2">
+          <li
+            key={"movie"}
+            onClick={() => {
+              setSelectedType("movie");
+            }}
+            className={`${selectedType === "movie" ? "bg-logo-blue text-logo-black" : "bg-logo-white/10"} hover:bg-logo-white/20 cursor-pointer snap-start rounded-full px-4 py-2 text-[clamp(.6rem,3vw,.8rem)] font-normal text-nowrap transition-colors duration-300 ease-in-out`}
+          >
+            Movie
+          </li>
+          <li
+            key={"tv"}
+            onClick={() => {
+              setSelectedType("tv");
+            }}
+            className={`${selectedType === "tv" ? "bg-logo-blue text-logo-black" : "bg-logo-white/10"} hover:bg-logo-white/20 cursor-pointer snap-start rounded-full px-4 py-2 text-[clamp(.6rem,3vw,.8rem)] font-normal text-nowrap transition-colors duration-300 ease-in-out`}
+          >
+            TV Show
+          </li>
+
+          {GenreList?.map((genre: GenreItem) => (
+            <li
+              key={genre.id}
+              onClick={() => {
+                setSelectedGenre(genre.id);
+              }}
+              className={`${genre.id === selectedGenre ? "bg-logo-blue text-logo-black shadow-logo-blue shadow-2xl" : "bg-logo-white/10"} hover:bg-logo-white/20 cursor-pointer snap-start rounded-full px-4 py-2 text-[clamp(.6rem,3vw,.8rem)] font-normal text-nowrap transition-colors duration-300 ease-in-out`}
+            >
+              {genre.name}
+            </li>
+          )) || "No categories available"}
+        </ul>
+      </div>
+
+      {/* loading state */}
+      {isLoading && <LoadingAnimation />}
+      {/* movies list */}
+      <div className="mt-5 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+        {data?.pages.map((page, i) => (
+          <React.Fragment key={i}>
+            {page.results.map((data: MediaItem) => (
+              <CategoryCard
+                key={data.id}
+                movie={{ ...data, type: selectedType }}
+              />
+            ))}
+          </React.Fragment>
+        ))}
+
+        <div className="h-[10] w-full" ref={loadMoreRef}></div>
+      </div>
+      <div className="h-20" />
     </div>
   );
 };
