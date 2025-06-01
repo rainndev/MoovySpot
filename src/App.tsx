ae31f4e0ac4a5280ee275e0157e7ba320f877c32
rainndev <182114243+rainndev@users.noreply.github.com>
2025-06-02 00:38:52 +0800

    refactor: lazy load page components for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import HomePage from "@/pages/HomePage";
 import Navigation from "@/components/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import VideoDetailsPage from "@/pages/VideoDetailsPage";
 import BottomNav from "@/components/BottomNav";
-import WatchlistPage from "@/pages/WatchlistPage";
-import RecentViewPage from "@/pages/RecentViewPage";
 import { useSearchModalStore } from "@/store/SearchModalStore";
 import SearchModal from "@/components/SearchModal";
-import PlayVideoPage from "@/pages/PlayVideoPage";
-import CategoryPage from "./pages/CategoryPage";
+import { lazy, Suspense } from "react";
+import LoadingAnimation from "./components/LoadingAnimation";
+
+const Homepage = lazy(() => import("@/pages/HomePage"));
+const WatchlistPage = lazy(() => import("@/pages/WatchlistPage"));
+const RecentViewPage = lazy(() => import("@/pages/RecentViewPage"));
+const CategoryPage = lazy(() => import("@/pages/CategoryPage"));
+const VideoDetailsPage = lazy(() => import("@/pages/VideoDetailsPage"));
+const PlayVideoPage = lazy(() => import("@/pages/PlayVideoPage"));
 
 const App = () => {
   const isSearchOpen = useSearchModalStore((state) => state.isOpen);
@@ -18,14 +21,16 @@ const App = () => {
       <div className="hide-scrollbar bg-logo-black text-logo-white h-dvh">
         <Navigation />
         <BottomNav />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/watchlist" element={<WatchlistPage />} />
-          <Route path="/recent" element={<RecentViewPage />} />
-          <Route path="/category" element={<CategoryPage />} />
-          <Route path="/details/:id" element={<VideoDetailsPage />} />
-          <Route path="/play/:id" element={<PlayVideoPage />} />
-        </Routes>
+        <Suspense fallback={<LoadingAnimation />}>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/watchlist" element={<WatchlistPage />} />
+            <Route path="/recent" element={<RecentViewPage />} />
+            <Route path="/category" element={<CategoryPage />} />
+            <Route path="/details/:id" element={<VideoDetailsPage />} />
+            <Route path="/play/:id" element={<PlayVideoPage />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
